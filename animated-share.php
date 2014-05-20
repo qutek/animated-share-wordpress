6c6d3628002c63ffe42270c6581c5843e8088911
Lafif Astahdziq <lafif@astahdziq.in>
2014-05-20 20:10:18 +0700

    modify: admin page

--- animated-share.php
+++ animated-share.php
@@ -32,26 +32,26 @@ function animated_share($content)
     }
 }
  
-function animated_share_style()
-{
-    echo '
-    <script type="text/javascript">
-        var plugUrl = "' . plugins_url('sharrre.php', __FILE__) . '/";
-    </script>';
-    // css yang akan di push ke wp_head
-    // wp_register_script('animated_share_style', plugins_url('css/animated-share.css', __FILE__));
-    wp_register_style( 'animated_share_style', plugins_url('css/animated-share.css', __FILE__) );
-    wp_register_script('jquery', plugins_url('js/jquery-1.7.min.js', __FILE__), false, '1.7');
-    wp_register_script('jquery_easing', plugins_url('js/jquery.easing.min.js', __FILE__), array(),'', false);
-    wp_register_script('sharrre', plugins_url('js/jquery.sharrre.js', __FILE__), array(),'', false);
-    wp_register_script('initialize', plugins_url('js/initialize.js', __FILE__), array(),'', true);
+// function animated_share_style()
+// {
+//     echo '
+//     <script type="text/javascript">
+//         var plugUrl = "' . plugins_url('sharrre.php', __FILE__) . '/";
+//     </script>';
+//     // css yang akan di push ke wp_head
+//     // wp_register_script('animated_share_style', plugins_url('css/animated-share.css', __FILE__));
+//     wp_register_style( 'animated_share_style', plugins_url('css/animated-share.css', __FILE__) );
+//     wp_register_script('jquery', plugins_url('js/jquery-1.7.min.js', __FILE__), false, '1.7');
+//     wp_register_script('jquery_easing', plugins_url('js/jquery.easing.min.js', __FILE__), array(),'', false);
+//     wp_register_script('sharrre', plugins_url('js/jquery.sharrre.js', __FILE__), array(),'', false);
+//     wp_register_script('initialize', plugins_url('js/initialize.js', __FILE__), array(),'', true);
 
-    wp_enqueue_style('animated_share_style');
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('jquery_easing');
-    wp_enqueue_script('sharrre');
-    wp_enqueue_script('initialize');
-}
+//     wp_enqueue_style('animated_share_style');
+//     wp_enqueue_script('jquery');
+//     wp_enqueue_script('jquery_easing');
+//     wp_enqueue_script('sharrre');
+//     wp_enqueue_script('initialize');
+// }
  
 
 function animated_share_settings()
@@ -88,11 +88,34 @@ function animated_share_settings()
 function animated_share_admin_menu()
 {
     // Tampilkan option di menu
-    add_options_page('Animated Share', 'Animated Share', 'manage_options', basename(__FILE__), 'animated_share_settings');
+    add_options_page('Animated Share', 'Animated Share', 'manage_options', plugin_dir_path( __FILE__ ) . 'setting.php');
 }
  
 add_action("the_content", "animated_share");
 add_action("admin_menu", "animated_share_admin_menu");
-add_action("wp_head", "animated_share_style");
- 
+// add_action("wp_head", "animated_share_style");
+
+
+function admin_assets() {
+    
+    wp_register_style( 'demo', plugins_url('assets/css/demo.css', __FILE__) );
+    wp_register_style( 'component', plugins_url('assets/css/component.css', __FILE__) );
+
+    wp_register_script('cbpFWTabs', plugins_url('assets/js/cbpFWTabs.js', __FILE__), array(),'', false);
+
+    wp_enqueue_style('demo');
+    wp_enqueue_style('component');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('cbpFWTabs');
+}
+
+add_action("admin_enqueue_scripts", "admin_assets");
+
+
+function admin_inline_js(){
+    echo "<script type='text/javascript'>\n";
+    echo "new CBPFWTabs( document.getElementById( 'tabs' ) );";
+    echo "\n</script>";
+}
+add_action( 'admin_print_footer_scripts', 'admin_inline_js' );
 ?>
